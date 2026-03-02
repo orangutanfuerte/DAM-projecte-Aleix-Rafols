982db7523bb88bfff5f99631ddc97c09e03a4903
Aleix Ràfols <arb31@alumnes.udl.cat>
2026-03-02 09:11:43 +0100

    Added windowinsets(0)

--- app/app/src/main/java/com/example/travellikeasigma/ui/screens/PlacesScreen.kt
+++ app/app/src/main/java/com/example/travellikeasigma/ui/screens/PlacesScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -108,6 +109,7 @@ fun PlacesScreen() {
     var placeToDelete: Place? by remember { mutableStateOf(null) }
 
     Scaffold(
+        contentWindowInsets = WindowInsets(0),
         topBar = {
             TopAppBar(
                 title = {
